41eeafae57518d5eafc244f641f0e8c3684f17a2
mitch000001 <mitch.wagna@gmail.com>
2011-03-16 16:37:17 +0100

    Set service class in async task

--- src/org/mitchwork/earthquake/EarthquakeService.java
+++ src/org/mitchwork/earthquake/EarthquakeService.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import org.w3c.dom.Document;
@@ -39,6 +40,7 @@ public class EarthquakeService extends Service {
 
     private Timer updateTimer;
     private float minimumMagnitude;
+    EarthquakeLookupTask lastLookup = null;
 
     @Override
     public void onCreate() {
@@ -102,80 +104,10 @@ public class EarthquakeService extends Service {
     }
 
     private void refreshEarthquakes() {
-    	// Get the XML
-    	URL url;
-    	try {
-    		String quakeFeed = getString(R.string.quake_feed);
-    		url = new URL(quakeFeed);
-
-    		URLConnection connection;
-    		connection = url.openConnection();
-
-    		HttpURLConnection httpConnection = (HttpURLConnection)connection;
-    		int responseCode = httpConnection.getResponseCode();
-
-    		if (responseCode == HttpURLConnection.HTTP_OK) {
-    			InputStream in = httpConnection.getInputStream();
-
-    			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    			DocumentBuilder db = dbf.newDocumentBuilder();
-
-    			// Parse the earthquake feed.
-    			Document dom = db.parse(in);
-    			Element docEle = dom.getDocumentElement();
-
-    			// Get a list of each earthquake entry.
-    			NodeList nl = docEle.getElementsByTagName("entry");
-    			if (nl != null && nl.getLength() > 0) {
-    				for (int i = 0; i < nl.getLength(); i++) {
-    					Element entry = (Element)nl.item(i);
-    					Element title = (Element)entry.getElementsByTagName("title").item(0);
-    					Element g = (Element)entry.getElementsByTagName("georss:point").item(0);
-    					Element when = (Element)entry.getElementsByTagName("updated").item(0);
-    					Element link = (Element)entry.getElementsByTagName("link").item(0);
-    					String details = title.getFirstChild().getNodeValue();
-    					String hostname = "http://earthquake.usgs.gov";
-    					String linkString = hostname + link.getAttribute("href");
-
-    					String point = g.getFirstChild().getNodeValue();
-    					String dt = when.getFirstChild().getNodeValue();
-    					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
-    					Date qdate = new GregorianCalendar(0, 0, 0).getTime();
-    					try {
-    						qdate = sdf.parse(dt);
-    					} catch (ParseException e) {
-    						e.printStackTrace();
-    					}
-
-    					String[] location = point.split(" ");
-    					Location l = new Location("dummyGPS");
-    					l.setLatitude(Double.parseDouble(location[0]));
-    					l.setLongitude(Double.parseDouble(location[1]));
-
-    					String magnitudeString = details.split(" ")[1];
-    					int end = magnitudeString.length()-1;
-    					double magnitude = Double.parseDouble(magnitudeString.substring(0, end));
-
-    					details = details.split(",")[1].trim();
-
-    					Quake quake = new Quake(qdate, details, l, magnitude, linkString);
-
-    					// Process a newly found earthquake
-    					addNewQuake(quake);
-    				}
-    			}
-    		}
-    	} catch (MalformedURLException e) {
-    		e.printStackTrace();
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	} catch (ParserConfigurationException e) {
-    		e.printStackTrace();
-    	} catch (SAXException e) {
-    		e.printStackTrace();
-    	}
-    	finally {
-      	}
+        if (lastLookup == null || lastLookup.getStatus().equals(AsyncTask.Status.FINISHED)) {
+            lastLookup = new EarthquakeLookupTask();
+            lastLookup.execute((Void[])null);
+        }
     }
 
     private void addNewQuake(Quake _quake) {
